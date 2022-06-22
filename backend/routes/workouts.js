edab35f3199f1a06a0e14899e6de20adc2064548
Dhoneil Angchangco <act.dangchangco@gmail.com>
2022-06-22 23:34:49 +0800

    minor changes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -9,8 +9,23 @@ router.get('/', function(req,res) {
 })
 
 //get a single workout
-router.get('/:id', function() {
+router.get('/:id', function(req,res) {
+    res.json({mssg:'get single workout'})
+})
 
+//post a new workout
+router.post('/',function (req,res) {
+    res.json({messg:'post a new workout'})
+})
+
+//delete a new workout
+router.delete('/:id',function (req,res) {
+    res.json({messg:'delete a workout'})
+})
+
+//update a new workout
+router.patch('/:id',function (req,res) {
+    res.json({messg:'update a workout'})
 })
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,19 @@ const workoutRoutes = require('./routes/workouts')
 //express app
 const app = express();
 
-//middleware
+//-------middleware(s)
+
+//this will parse all data into json before throwing to the server
+app.use(express.json())
+
 //this function will fire for every request that comes in
 app.use((req,res,next)=>{
     console.log(req.path, req.method)
     next()
 });
+
+
+//-------middleware(s)
 
 //ROUTES
 app.use('/api/workouts/',workoutRoutes)
